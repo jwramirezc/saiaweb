867d18777b65520ac5a8b0cc8170f95557e82f4c
Jorge Ramirez <jwramirezc@gmail.com>
2024-07-17 08:43:53 -0500

    inicio de pagina de precios

---
+++ precios.js
@@ -0,0 +1,33 @@
+
+// Creando los servicios
+
+function ModulosSaia (nombreModulo, planModulo, valorModulo, alcancesModulo){
+    this.nombreModulo = nombreModulo
+    this.planModulo = planModulo
+    this.valorModulo = valorModulo
+    this.alcancesModulo = alcancesModulo
+    this.tieneDescuento = false
+    this.displayInfo = function (){
+        console.log(`Información del módulo:
+
+        Módulo: ${this.nombreModulo}
+        Valor: ${this.planModulo}
+        Alcances: ${this.alcancesModulo}
+        Descuento: ${this.tieneDescuento ? 'Con descuento del 30%':'Sin descuento'} 
+        `)
+    }
+    this.aplicarDescuento = function (){
+        this.tieneDescuento = true
+        console.log(`El módulo ${this.nombreModulo} tiene el descuento.`)
+        }
+}
+
+const correspondencia = new ModulosSaia ('Correspondencia','Basic','US$ 50','Recepción y Despacho, Múltiples Ventanillas, Recepción de Emails, Ventanilla Virtual, Codificación QR')
+const gestionDocumental = new ModulosSaia ('Gestión Documental','Standard','US$ 150', 'Formatos de Gestión, Rutas de aprobación, Firma electrónica, Modelador de Formatos, Comprobación de lectura')
+const archivo = new ModulosSaia ('Archivo','Standard','US$ 100','Estandarización de archivo, Archivo Físico, Custodia, Préstamos y Reservas, Reportes')
+
+gestionDocumental.aplicarDescuento()
+
+correspondencia.displayInfo()
+gestionDocumental.displayInfo()
+archivo.displayInfo()
