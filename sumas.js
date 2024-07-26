4c3ec04ae14f94bdad906dcd8dd5633b56c849ae
Jorge Ramirez <jwramirezc@gmail.com>
2024-07-25 19:20:57 -0500

    se agrega cuadrante de personalidad

---
+++ sumas.js
@@ -0,0 +1,18 @@
+
+
+
+// Capturamos el elemento select
+let selectElement = document.getElementById('opciones');
+let control = [];
+
+// Añadimos un evento para capturar el valor seleccionado cada vez que cambie
+selectElement.addEventListener('change', function() {
+    let valorSeleccionado = selectElement.value;
+    console.log('Valor seleccionado:', valorSeleccionado);
+    
+});
+
+
+if (condition) {
+    
+}
