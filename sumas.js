7eee112554dfbd7511ba7ceab1d74742639782cd
Jorge Ramirez <jwramirezc@gmail.com>
2024-07-26 16:05:15 -0500

    validacion de mostrar solo 1 resultado después del cálculo

--- sumas.js
+++ sumas.js
@@ -1,18 +1,12 @@
-
-
-
 // Capturamos el elemento select
 let selectElement = document.getElementById('opciones');
 let control = [];
 
 // Añadimos un evento para capturar el valor seleccionado cada vez que cambie
-selectElement.addEventListener('change', function() {
-    let valorSeleccionado = selectElement.value;
-    console.log('Valor seleccionado:', valorSeleccionado);
-    
+selectElement.addEventListener('change', function () {
+  let valorSeleccionado = selectElement.value;
+  console.log('Valor seleccionado:', valorSeleccionado);
 });
 
-
 if (condition) {
-    
 }
